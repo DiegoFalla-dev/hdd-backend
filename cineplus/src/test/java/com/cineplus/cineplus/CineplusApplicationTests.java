094b5663c7e17b5c399dcd1755d0d6907bcb2436
yosriel <diego.falla.2004.10@gmail.com>
2025-10-16 12:54:05 -0500

    sprinf

---
+++ cineplus/src/test/java/com/cineplus/cineplus/CineplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineplus.cineplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
